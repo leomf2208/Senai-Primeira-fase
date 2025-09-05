c5d15fa4986969f29a5bb80acd6cbd62455a52a1
leomf2208 <leomf.9@gmail.com>
2025-09-04 21:53:29 -0300

    atualização java

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,26 @@
-alert("ola mundo!")
+// alert("ola mundo!")
+
+// alert("oi")
+
+// alert("bem vindo ao java")
+
+// alert("tudo bem?")
+
+// alert("voce vem sempre aqui")
+
+// let idade 
+
+// idade = 18
+// alert(idade)
+
+// idade = "melancia"
+// alert(idade)
+
+const amor = "meu primeiro amor"
+alert(amor)
+
+alert(amor2)
+let amor2 = "meu segndo amor"
+
+var antigao = "nunca usar"
 

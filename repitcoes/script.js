9fdf2e53c91606dce04daaf7e5db35f7cafac09c
leomf2208 <leomf.9@gmail.com>
2025-09-29 22:15:55 -0300

    atualizacao da aula

---
+++ repitcoes/script.js
@@ -0,0 +1,13 @@
+// let acertos = Number(prompt("Digite o seu acerto: "))
+// let valorPorQuestao = 10/16
+// let nota = acertos * valorPorQuestao
+
+// alert(`a nota do aluno foi ${nota}`)
+// alert("a nota do aluno " + nota)
+
+let contador = 0
+while(contador < 2){
+    // alert("contador: " + contador)
+    contador = contador + 1
+    console.log("contador: " + contador)
+}
